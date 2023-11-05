71b6cef41fe8867dd88ccdf7f791c2335a2dd2be
Bamnawatjitesh <jiteshmeena1748@gmail.com>
2023-11-05 23:05:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+import Result from "./components/Result";
+
+// dummy question we can use API also to fetch questions one by one
+// or a complete dump at once
+const questions = [
+  {
+    question: "What is the capital of France?",
+    options: ["New York", "London", "Paris", "Dublin"],
+    answer: "Paris",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    options: [
+      "Vincent Van Gogh",
+      "Pablo Picassos",
+      "Leonardo Da Vinci",
+      "Claude Monet",
+    ],
+    answer: "Leonardo Da Vinci",
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    options: ["Au", "Ag", "Fe", "Cu"],
+    answer: "Au",
+  },
+  {
+    question:
+      "Which gas do plants absorb from the atmosphere during photosynthesis?",
+    options: ["Oxygen", "Nitrogen", "Carbon Dioxide", "Hydrogen"],
+    answer: "Carbon Dioxide",
+  },
+  {
+    question: "Who was the first woman to fly solo across the Atlantic Ocean?",
+    options: [
+      "Amelia Earhart",
+      "Bessie Coleman",
+      "Harriet Quimby",
+      "Jacqueline Cochran",
+    ],
+    answer: "Amelia Earhart",
+  },
+  {
+    question: "In which year did the Titanic sink?",
+    options: ["1912", "1921", "1935", "1944"],
+    answer: "1912",
+  },
+  {
+    question: "What is the largest mammal on Earth?",
+    options: ["African Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+    answer: "Blue Whale",
+  },
+  {
+    question: "Who is the author of 'The Great Gatsby'?",
+    options: [
+      "F. Scott Fitzgerald",
+      "Ernest Hemingway",
+      "Mark Twain",
+      "John Steinbeck",
+    ],
+    answer: "F. Scott Fitzgerald",
+  },
+  {
+    question: "What is the chemical formula for water?",
+    options: ["H2O", "CO2", "N2", "CH4"],
+    answer: "H2O",
+  },
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["Mars", "Venus", "Mercury", "Jupiter"],
+    answer: "Mars",
+  },
+];
+
+const Answers = {};
+
+function App() {
+  const [showResult, setshowResult] = useState(false);
+
+  return (
+    <div className="App h-screen">
+      {!showResult ? (
+        <Dashboard
+          questions={questions}
+          Answers={Answers}
+          setshowResult={setshowResult}
+        />
+      ) : (
+        <Result questions={questions} Answers={Answers} />
+      )}
+    </div>
+  );
+}
+
+export default App;
